10deab568f0f4be19aea0074346ae73062610fe8
Mikeul <56322741+Mikeultron@users.noreply.github.com>
2020-05-07 04:05:56 +0700

    Added comments to Player class.

--- BlackJack/Player.cs
+++ BlackJack/Player.cs
@@ -3,12 +3,18 @@ namespace BlackJack
 {
     class Player : Base
     {
+        // To hold total value of player's cards.
         public static int value { get; set; }
+        
+        // override the base's class method.
+        // Generate a new random card and ad it to the list.
         public override void Hit()
         {
             GamePlay.playerCard.Add(Card.GenerateCard());
             Card.ShowCard("player");
         }
+        
+        // Method for handling player's turn.
         public void PlayerTurn()
         {
             Console.WriteLine("Hit or stand? ");
