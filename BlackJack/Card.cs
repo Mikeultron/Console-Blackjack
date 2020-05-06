e085a6f0b405a9cef758058282c81a61e48cf922
Mikeul <56322741+Mikeultron@users.noreply.github.com>
2020-05-07 04:00:57 +0700

    Added comments

--- BlackJack/Card.cs
+++ BlackJack/Card.cs
@@ -3,13 +3,19 @@ namespace BlackJack
 {
     public class Card
     {
+        // Initialize the card value.
         public static string[] cards = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+        // Initialize the card shapes.
         public static string[] cardShapes = {"Heart", "Spade", "Club", "Diamond"};
+        
+        // Generate a random card.
         public static string GenerateCard()
         {
             Random rand = new Random();
             return $"{cards[rand.Next(0, 13)]} {cardShapes[rand.Next(0, 4)]}";
         }
+        
+        // Show the card of the specified name.
         public static void ShowCard(string name)
         {
             if(name == "player")
